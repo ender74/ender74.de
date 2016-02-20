ac76556635b02205ca87f1b75270d4e1be94a7f2
Heiko Hüter <heiko.hueter@novasib.de>
2016-02-20 12:10:42 +0100

    removed unnecessary parameter to uglify

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,30 +10,32 @@ import babelify from 'babelify'
 import streamify from 'gulp-streamify'
 
 var path = {
-  HTML: 'src/static/index.html',
-  STATIC: ['src/static/**/*.*'],
-  ALL: ['src/**/*.js', 'src/static/**/*.*'],
-  ENTRY_POINT: ['src/main.js'],
-  MINIFIED_OUT: 'build.min.js',
-  DEST_SRC: 'dist/src',
-  DEST_BUILD: 'dist/',
-  DEST: 'dist'
+    HTML: 'src/static/index.html',
+    STATIC: ['src/static/**/*.*'],
+    ALL: ['src/**/*.js', 'src/static/**/*.*'],
+    ENTRY_POINT: ['src/main.js'],
+    MINIFIED_OUT: 'build.min.js',
+    DEST_SRC: 'dist/src',
+    DEST_BUILD: 'dist/',
+    DEST: 'dist'
 }
 
-gulp.task('copy', function(){
-  gulp.src(path.STATIC)
-    .pipe(gulp.dest(path.DEST));
+gulp.task('copy', function () {
+    gulp.src(path.STATIC)
+        .pipe(gulp.dest(path.DEST));
 })
 
-gulp.task('build', function(){
-  browserify({
-    entries: [path.ENTRY_POINT],
-    transform: [babelify]
-  })
-    .bundle()
-    .pipe(source(path.MINIFIED_OUT))
-    .pipe(streamify(uglify(path.MINIFIED_OUT)))
-    .pipe(gulp.dest(path.DEST_BUILD));
+gulp.task('build', function () {
+    browserify({
+        entries: [path.ENTRY_POINT]
+    }).
+        transform(babelify.configure({
+            presets: ["es2015", "react"]
+        }))
+        .bundle()
+        .pipe(source(path.MINIFIED_OUT))
+        .pipe(streamify(uglify()))
+        .pipe(gulp.dest(path.DEST_BUILD));
 })
 
 gulp.task('release', ['copy', 'build']);
